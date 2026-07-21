c6739e4fed5ef21814a7ae369dedd3563ca4f3a6
Derk101 <chukwudubemchukwuma@gmail.com>
2026-07-21 10:15:31 -0400

    Implement removeItem and updateQuantity reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -25,10 +25,20 @@ export const CartSlice = createSlice({
         }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(
+            item => item.name !== action.payload
+        );
     },
     updateQuantity: (state, action) => {
-
+        const { name, quantity } = action.payload;
     
+        const itemToUpdate = state.items.find(
+            item => item.name === name
+        );
+    
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
