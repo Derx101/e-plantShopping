c9b931773b4d6a9642da54e2793e32e62e35adb8
Derk101 <chukwudubemchukwuma@gmail.com>
2026-07-21 11:31:19 -0400

    Complete Task 4 Redux integration

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -29,12 +29,7 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
-    dispatch(
-      updateQuantity({
-        name: item.name,
-        quantity: item.quantity + 1,
-      })
-    );
+    dispatch(addItem(item));
   };
 
   const handleDecrement = (item) => {
@@ -49,6 +44,7 @@ const CartItem = ({ onContinueShopping }) => {
       dispatch(removeItem(item.name));
     }
   };
+  
  const handleRemove = (item) => {
   dispatch(removeItem(item.name));
 };
